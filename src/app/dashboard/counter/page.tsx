91cea94c944dc45431f6cdeaf98970dc22e4826c
lucjuare <lujuarez@emeal.nttdata.com>
2024-01-07 22:30:32 -0300

    build components from page for use client only in component

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,18 @@
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata = {
+    title: "Shopping Cart",
+    description: "Counter Client Side",
+}
 
 export default function CounterPage() {
+
+    // aca puede haber una conexion a base de datos o un fetch que se ejecute en el servidor
+
     return (
-        <div>
-            <h1> Page Counter </h1>
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <span> Productos en el carrito</span>
+            <CartCounter value={50} />
         </div>
     );
 }
