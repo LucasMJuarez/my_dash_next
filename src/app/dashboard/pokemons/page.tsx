cc7c07bf73c46e2e42541c540fc8aa5b0b9bb311
lucjuare <lujuarez@emeal.nttdata.com>
2024-01-18 01:34:14 -0300

    componenets grid and card pokemons

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,4 +1,4 @@
-import { PokemonsResponse, SimplePokemon } from "@/app/pokemons"
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons"
 import Image from "next/image"
 
 const getPokemons = async (limit = 10, offset = 0): Promise<SimplePokemon[]> => {
@@ -22,20 +22,9 @@ export default async function PokemonsPage() {
     return (
         <div className="flex flex-col">
 
-            <div className="flex flex-wrap gap-10 items-center justify-center">
-                {pokemons.map((pokemon) => (
+            <span className="text-5xl my-2">Listado de Pokemons <small>Estático</small></span>
 
-                    <Image
-                        key={pokemon.id}
-                        className="rounded-full w-8 h-8"
-                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-                        alt={pokemon.name}
-                        width={100}
-                        height={100}
-                    />
-
-                ))}
-            </div>
+            <PokemonGrid pokemons={pokemons} />
         </div>
     );
 }
