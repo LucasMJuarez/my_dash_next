4bd111dd8a4f8ff1aa6145e6af502bcacc2c9a3a
lucjuare <lujuarez@emeal.nttdata.com>
2024-01-18 03:19:35 -0300

    add interfaces for pokemons and fetch for id

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -2,3 +2,4 @@ export { PokemonGrid } from '../pokemons/components/PokemonGrid'
 
 export type { PokemonsResponse } from './interfaces/pokemons-response'
 export type { SimplePokemon } from './interfaces/simple-pokemons'
+export type { Pokemon } from './interfaces/pokemon'
