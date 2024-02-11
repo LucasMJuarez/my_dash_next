1faa8befb235edae87fa512d9832367363f4653a
lucasMJuarez <lumas89@gmail.com>
2024-02-11 14:47:59 -0300

    add toggle favorites reducer and call from dispatch in pokemon-cards

--- src/store/pokemons/pokemons.ts
+++ src/store/pokemons/pokemons.ts
@@ -1,5 +1,5 @@
 import { SimplePokemon } from '@/pokemons';
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice , PayloadAction} from '@reduxjs/toolkit'
 
 /*
  {
@@ -16,14 +16,27 @@ interface PokemonsState {
 
 const initialState: PokemonsState = {
     '1': { id: '1', name: 'bulbasaur' },
+    '3': { id: '3', name: 'venusaur' },
 }
 
 const pokemonsSlices = createSlice({
     name: 'pokemons',
     initialState,
-    reducers: {}
+    reducers: {
+        toggleFavorite(state, action: PayloadAction<SimplePokemon>) {
+            const pokemon = action.payload;
+            const {id} = pokemon;
+            if(!!state[id]){
+                delete state[id];
+                return
+            }
+
+            state[id] = pokemon;
+        }
+
+    }
 });
 
-export const { } = pokemonsSlices.actions
+export const { toggleFavorite } = pokemonsSlices.actions
 
 export default pokemonsSlices.reducer
