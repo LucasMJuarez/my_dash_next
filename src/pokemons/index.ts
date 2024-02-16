b91d19c362d86c09e74f74fcd384e44a3fe0cb4c
lucasMJuarez <lumas89@gmail.com>
2024-02-16 03:27:06 -0300

    add componente favorites and store use

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,4 +1,5 @@
 export { PokemonGrid } from '../pokemons/components/PokemonGrid'
+export { FavoritePokemons } from '../pokemons/components/FavoritePokemons'
 
 export type { PokemonsResponse } from './interfaces/pokemons-response'
 export type { SimplePokemon } from './interfaces/simple-pokemons'
